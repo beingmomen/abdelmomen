15c0b3c88257c8243d8d9f429629746ce72cc42f
Abdelmo’men Elshatory <abdelmomen.elshatory@nanosoft.ly>
2022-06-29 12:59:58 +0200

    push

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -7,6 +7,42 @@
 <script>
 export default {
   layout: "admin",
+  async asyncData({ $fire, store }) {
+    await $fire.firestore
+      .collection("skills")
+      .get()
+      .then((res) => {
+        store.dispatch("dashboard/skills/getAllDataFromApi", res);
+      });
+
+    await $fire.firestore
+      .collection("projects")
+      .get()
+      .then((res) => {
+        store.dispatch("dashboard/projects/getAllDataFromApi", res);
+      });
+
+    await $fire.firestore
+      .collection("companies")
+      .get()
+      .then((res) => {
+        store.dispatch("dashboard/companies/getAllDataFromApi", res);
+      });
+
+    await $fire.firestore
+      .collection("messages")
+      .get()
+      .then((res) => {
+        store.dispatch("dashboard/messages/getAllDataFromApi", res);
+      });
+
+    await $fire.firestore
+      .collection("hireMe")
+      .get()
+      .then((res) => {
+        store.dispatch("dashboard/hireMe/getAllDataFromApi", res);
+      });
+  },
   data() {
     return {};
   },
